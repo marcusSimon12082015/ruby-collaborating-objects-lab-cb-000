4063e0fa9aff4cbf853120594b45365d4568d409
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-02 17:53:30 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,9 +1,11 @@
 class MP3Importer
   attr_accessor :path
   @files = []
-  
+
   def initialize(file_path)
     @path = file_path
   end
   def files
+    @files = Dir.entires(@path).select{|file| file.extname == ".mp3"}
+  end
 end
