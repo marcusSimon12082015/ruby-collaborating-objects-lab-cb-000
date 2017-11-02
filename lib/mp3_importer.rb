011689be5038693c05c0dc944f4a86744e5db1e0
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-02 18:20:08 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,11 +1,12 @@
 class MP3Importer
   attr_accessor :path, :files
+  @@all = []
 
   def initialize(file_path)
     @path = file_path
     @files = Dir.entries(@path).select{|file| file.include?(".mp3")}
   end
   def import
-
+    @@all = @files.uniq
   end
 end
