70c3763f3b89672d39d7c49dfc1f47408d8d04d3
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-02 18:41:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,7 @@ class Song
   def self.new_by_filename(filename)
     split_array = filename.split(" - ")
     song = Song.new(split_array[1])
+    puts "#{song.title}"
     song.artist = Artist.find_or_create_by_name(split_array[0]).add_song(song)
     puts "#{song.artist}"
     song
